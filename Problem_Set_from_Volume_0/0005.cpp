cd74867135f06c96738157213615408cc31dc905
ちょまど <chomado@gmail.com>
2014-08-07 11:28:40 +0900

    Create 0005.cpp
     最大公約数と最小公倍数の問題。http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0005&lang=jp

---
+++ Problem_Set_from_Volume_0/0005.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+int Gcd(int a, int b) // Greatest Common Divisor
+{
+    if (!a || !b) {
+        return 0;
+    }
+    while (a != b) {
+        if (a < b) 
+            std::swap(a, b);
+        a -= b;
+    }
+    return a;
+}
+int Lcm(int a, int b, int gcd) // Least Common Multiple
+{
+    if (!a || !b) {
+        return 0;
+    }
+    return a * (b / gcd); // 桁あふれ防止のために計算順序考慮
+}
+int main()
+{
+    int a, b, gcd;
+    while (std::cin >> a >> b) {
+        gcd = Gcd(a, b);
+        std::cout << gcd << " " << Lcm(a, b, gcd) << "\n";
+    }
+}
