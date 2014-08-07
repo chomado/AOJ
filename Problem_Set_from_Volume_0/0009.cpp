3cfd3f33bf6a0ae1dce0e7018a0e690f7769933b
ちょまど <chomado@gmail.com>
2014-08-07 11:32:10 +0900

    Create 0009.cpp (countup_primes)
    http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0009&lang=jp

---
+++ Problem_Set_from_Volume_0/0009.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+ 
+//素数かどうかが真のものの数（カウントアップ）を返す
+int countup(vector<bool>::iterator begin, vector<bool>::iterator end) 
+{
+    int counter = 0;
+    while (begin++ != end) {
+        if (*begin) 
+            counter++;
+    }
+    return counter;
+}
+int main()
+{
+    int n;
+    
+    while (cin >> n) {
+        int                     max = sqrt(static_cast<double>(n));
+        vector<bool>            is_prime(n, true);
+        vector<bool>::iterator  begin, end;
+ 
+        is_prime[0] = is_prime[1] = false;
+        
+        for (int i=2; i<=max; i++) {
+            if (!is_prime[i]) {
+                continue;
+            }
+            else { // iがもし素数だったら、(自分以外の)その倍数全部潰す
+                for (int cntr = 2, j = 2*i; j<=n; j = i*(cntr++)) { 
+                    is_prime[j] = false;
+                }
+            }
+        }
+        begin   = is_prime.begin();
+        end     = is_prime.end();
+        cout << countup(begin, end) << endl;
+    }
+}
