c48f530a932b8673f2affc86d374ce9d214d0c42
ちょまど <chomado@gmail.com>
2014-08-28 11:17:46 +0900

    Update [C++11]_0029_EnglishSentence.cpp

--- Problem_Set_from_Volume_0/[C++11]_0029_EnglishSentence.cpp
+++ Problem_Set_from_Volume_0/[C++11]_0029_EnglishSentence.cpp
@@ -5,16 +5,14 @@
 #include <algorithm>
 int main()
 {
-	typedef std::map<std::string, int>	simap;
-	typedef std::pair<std::string, int> 	sipair;
-	simap 		words; 
-	std::string 	input;
+	std::map<std::string, int>	words;
+	std::string 			input;
 
 	while (std::cin >> input) 
 		++words[input]; 
 	
-	auto result  = std::minmax_element(words.begin(), words.end(), [](const sipair & a, const sipair & b) { return a.first.length() < b.first.length(); });
-	auto result2 = std::minmax_element(words.begin(), words.end(), [](const sipair & a, const sipair & b) { return a.second < b.second; });
+	auto result  = std::minmax_element(words.begin(), words.end(), [](const auto & a, const auto & b) { return a.first.length() < b.first.length(); });
+	auto result2 = std::minmax_element(words.begin(), words.end(), [](const auto & a, const auto & b) { return a.second < b.second; });
 
 	std::cout << result2.second->first << ' ' << result.second->first << std::endl;
 }
